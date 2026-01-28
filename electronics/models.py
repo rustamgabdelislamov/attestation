6a14af6859a6795a01ec219f5426fa78f2c68199
Rustam Gabdelislamov <rustamgabdelislamov911@gmail.com>
2026-01-28 09:08:26 +0500

    init electronics

---
+++ electronics/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+
+class Contact(models.Model):
+    email = models.EmailField(unique=True, verbose_name="Email")
+    country = models.CharField(max_length=255, verbose_name="Страна")
+    city = models.CharField(max_length=255, verbose_name="Город")
+    street = models.CharField(max_length=255, verbose_name="Улица")
+    house_number = models.CharField(max_length=10, verbose_name="Дом")
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255, verbose_name="Имя")
+    model = models.CharField(max_length=255, verbose_name="Модель")
+    release_date = models.DateField(verbose_name="Дата выхода продукта на рынок")
+
+
+class Organization(models.Model):
+    name = models.CharField(max_length=255)
+    contact = models.OneToOneField(Contact, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product)
+    supplier = models.ForeignKey(
+        "self",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="customers",
+        verbose_name="Поставщик",
+        help_text="Введите поставщика. Если поставщик не указан, данная организация автоматически считается заводом.",
+    )
+    debt_to_supplier = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
